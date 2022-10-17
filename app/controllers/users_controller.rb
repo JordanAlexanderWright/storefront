33336620ad91c34fe886800695a100f806e51b67
Jordan Wright <jaws610@gmail.com>
2022-10-17 14:40:57 -0400

    Creating User Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
   def index 
-    
+    @user = User.new
+  end
+
+  def new 
+    @user = User.new
+  end
+
+  def create 
+    p params 
+    p params[:user]
+    @user = User.new(user_params)
+    p 'HELLO WORLD'
+    p params
+
+    if @user.save 
+      flash[:notice] = "User Created"
+      redirect_to action: "index"
+    else
+      flash[:notice] = "There's a problem with your data"
+    end
+  end
+
+  private 
+  def user_params 
+    params.require(:user).permit(:first_name, :last_name, :user_name, :password, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { maximum: 16 } 
-  validates :last_name, presence: true, length: { maximum : 16 }
-  validates :user_name, presence: true, length: { in 4..20 }
-  validates :password, presence: true, length: { in 6..20 }
-  validates :email, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true, length: { maximum: 16 }
+  validates :user_name, presence: true, length: { in: 4..20 }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
 end
